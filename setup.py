ced05850c5bdb8b97c8c3d3c8466848b786f152d
Pon-ek <121844165+ponekscb@users.noreply.github.com>
2023-07-06 21:30:38 +0700

    Fix cicd (#1)
    * Fix gitflow deploy.yml for artifact registry
    
    * Remove version py3.6
    
    * Remove gcloud auth activate-service-account, Update google-github-actions/auth
    
    * Fix gcloud .pypirc
    
    * Add gcloud config set account

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     test_suite='',
     classifiers=[
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
